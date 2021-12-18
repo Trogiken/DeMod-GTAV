75302dc711681c9455755fece72ff9dd80c1832a
Trogiken <noah@blaszak.com>
2021-12-18 12:57:46 -0500

    Verify File Locations Builder will only continue if it confirmed all required assets are found

--- pre-builder.py
+++ pre-builder.py
@@ -20,3 +20,39 @@ while True:
     else:
         input("Failed to Verify PyInstaller Status\nPress ENTER to exit")
         exit()
+
+# Verify File Locations
+cwd = os.getcwd()
+app = f'{cwd}\\app'
+updater = f'{cwd}\\updater'
+verify = True
+
+app_required = ['main', 'mode_edit', 'class_animation', 'class_threadloading']
+compare_app = []
+app_files = os.listdir(app)
+for file in app_files:
+    if file in app_required:
+        compare_app.append(file)
+if app_required != compare_app:  # If all required files aren't found
+    verify = False
+    for file in app_required:
+        if file in compare_app:
+            app_required.remove(file)  # Leave remaining files that haven't been found
+    print(f'Required files Missing from: {app}\nMissing Files [{app_required}]')
+
+updater_required = ['updater']
+compare_updater = []
+updater_files = os.listdir(updater)
+for file in updater_files:
+    if file in updater_required:
+        compare_updater.append(file)
+if updater_required != compare_updater:  # If all required files aren't found
+    verify = False
+    for file in updater_required:
+        if file in compare_updater:
+            updater_required.remove(file)  # Leave remaining files that haven't been found
+    print(f'Required files Missing from: {updater}\nMissing Files [{updater_required}]')
+
+if verify is False:  # If any of the required files are missing
+    input('Press ENTER to exit')
+    exit()
